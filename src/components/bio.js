e5b92cf55e3444a8e3fee7297fa2dca11b799143
panosmakris <53366987+panosmakris@users.noreply.github.com>
2019-09-17 17:46:44 +0300

    da

--- src/components/bio.js
+++ src/components/bio.js
@@ -54,11 +54,8 @@ const Bio = () => {
         }}
       />
       <p>
-        Written by <strong>{author}</strong> who lives and works in San
-        Francisco building useful things.
-        {` `}
-        <a href={`https://twitter.com/${social.twitter}`}>
-          You should follow him on Twitter
+        <a href="https://www.linkedin.com/in/panos-makris-8aa02a27/">
+          Βρες με στο linkedin
         </a>
       </p>
     </div>
