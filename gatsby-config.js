ef9c971cf4c7f736da4a8cb15892755a3dc15b34
panosmakris <53366987+panosmakris@users.noreply.github.com>
2020-01-04 23:33:16 +0200

    sa

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,13 +51,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Performance Marketing Blog`,
+        name: `Performance Marketing Athens`,
         short_name: `GatsbyJS`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/dns_2017.png`, 
       },
     },
     `gatsby-plugin-offline`,
