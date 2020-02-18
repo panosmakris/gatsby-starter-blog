d266cf18377bafcd1d45cae4e071e44adeda20c9
panosmakris <53366987+panosmakris@users.noreply.github.com>
2020-02-18 14:41:57 +0200

    remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,11 +27,10 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
+          `gatsby-remark-relative-images`,
           {
             resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 590,
-            },
+            options: {},
           },
           {
             resolve: `gatsby-remark-responsive-iframe`,
