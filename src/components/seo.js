11f71348375e377e2faab6b7717f1528055449b9
panosmakris <53366987+panosmakris@users.noreply.github.com>
2020-02-18 15:15:11 +0200

    Update seo.js

--- src/components/seo.js
+++ src/components/seo.js
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-function SEO({ description, lang, meta, title, image }) {
+function SEO({ description, lang, meta, title, image, thumbnail }) {
   const { site } = useStaticQuery(
     graphql`
       query {
