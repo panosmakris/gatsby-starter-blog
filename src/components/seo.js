115568a29cddcd8dc015633fa6e35440253ad0d2
panosmakris <53366987+panosmakris@users.noreply.github.com>
2020-02-19 11:03:17 +0200

    Update seo.js

--- src/components/seo.js
+++ src/components/seo.js
@@ -48,7 +48,7 @@ function SEO({ description, lang, meta, title, thumbnail }) {
           content: metaDescription,
         },
         { property: `og:image`,
-          content: thumbnail,
+          content: `thumbnail`,
         },
         {
           property: `og:type`,
@@ -67,7 +67,7 @@ function SEO({ description, lang, meta, title, thumbnail }) {
           content: title,
         },
         { name: `twiiter:image`,
-          content: thumbnail,
+          content: `thumbnail`,
         },
         {
           name: `twitter:description`,
